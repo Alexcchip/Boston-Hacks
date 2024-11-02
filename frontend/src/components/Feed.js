36d56091b1075b1ddf4e929e197bcb6f38ebb645
Alex Weinberger <alexcchip@gmail.com>
2024-11-02 16:04:06 -0400

    astro photo stuff

---
+++ frontend/src/components/Feed.js
@@ -0,0 +1,17 @@
+// Button.js
+import React from 'react';
+import Post from './Post';
+  
+function Feed({ posts }) {
+  return (
+    <div className="min-h-screen flex flex-col items-center justify-center bg-orange-200 pt-20">
+      {posts.map((post) => (
+        <div key={post.post_id} className="mb-4">
+          <Post post={post} />
+        </div>
+      ))}
+    </div>
+  );
+}
+
+export default Feed;
--- frontend/src/components/post.js
+++ frontend/src/components/post.js
@@ -1,19 +1,15 @@
 // Button.js
 import React from 'react';
-
-function Button({ text }) {
-  const handleClick = () => {
-    alert(`Button clicked!`);
-  };
-
+  
+function Post({ post }) {
   return (
-    <button
-      onClick={handleClick}
-      className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
-    >
-      {text}
-    </button>
+    <div className="border p-4 rounded-lg shadow-md bg-white max-w-xs sm:max-w-sm md:max-w-md w-full mx-auto">
+      <h2 className="text-lg font-bold">{post.username}</h2>
+      <p className="text-gray-600">{post.caption}</p>
+      <img src={post.photo} alt="post" className="mt-2 w-full h-auto rounded" />
+      <p className="text-sm text-gray-500">{post.posted_at.toLocaleString()}</p>
+    </div>
   );
 }
 
-export default Button;
+export default Post;
