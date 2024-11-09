59fba37f74e7e21b8e57dc403cdc7eca5059b6f0
nourshoreibah <nour17372@icloud.com>
2024-11-09 13:40:05 -0500

    cors fix

--- backend/app.py
+++ backend/app.py
@@ -26,7 +26,7 @@ def create_app():
     app = Flask(__name__)
     
     # Configure CORS
-    CORS(app, resources={r"/*": {"origins": "*"}}, allow_headers="*")
+    CORS(app, resources={r"/*": {"origins": "*", "supports_credentials": True, "allow_headers": ["Content-Type", "Authorization"]}})
 
     
     # Database Configuration for PostgreSQL
